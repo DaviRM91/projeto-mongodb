a4665a649f26017eea37f7d1b3eade8bba6d862d
DaviRM1 <davi.rm@outlook.com>
2026-01-19 14:01:39 -0300

    Conectando ao MongoDB com repository e service

---
+++ workshopmongo/src/main/java/com/davi/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.davi.workshopmongo.repository;
+
+import com.davi.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
