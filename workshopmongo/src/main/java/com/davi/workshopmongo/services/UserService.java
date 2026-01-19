2f936dac63c3bb7287eda38ed897605d45b5c299
DaviRM1 <davi.rm@outlook.com>
2026-01-19 16:20:46 -0300

    Obtendo um usuário por id

--- workshopmongo/src/main/java/com/davi/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/davi/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.davi.workshopmongo.services;
 
 import com.davi.workshopmongo.domain.User;
 import com.davi.workshopmongo.repository.UserRepository;
+import com.davi.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
     public List<User> findAll(){
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 
 }
